dda155c33046a4ca770e4e2428247f102267e1fb
terrancybernetics <147798517+terrancybernetics@users.noreply.github.com>
2024-10-24 18:34:41 -0400

    server loads map

--- dedicatedserver.js
+++ dedicatedserver.js
@@ -1,7 +1,12 @@
 export class DedicatedServer {
   constructor(io){
 
+    import initJolt from '/jolt-physics.wasm-compat.js';
+    import { Ent } from '/ent.js';
+    import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+    const loader = new GLTFLoader();
     this.list = [];
+    var scene = new THREE.Scene();
 
     io.listen(3000) // default port is 9208
 
@@ -40,9 +45,76 @@ export class DedicatedServer {
       })
     })
 
-    this.onConnection = function(io, channel) {
-      
+    function initGraphics() {
+
     }
+
+    function initPhysics() {
+
+    }
+
+    function initExample(Jolt, updateFunction) {
+      initGraphics();
+      initPhysics();
+      //setImmediate(function() {})
+    }
+
+    initJolt().then(function (Jolt) {});
+
+      loader.load(
+        'map.glb',
+        // called when the resource is loaded
+        function ( gltf ) {
+          initExample(Jolt, null);
+          scene.add( gltf.scene );
+
+          for (var m = 0; m < gltf.scene.children.length; m++) {
+
+              if(gltf.scene.children[m].name.slice(-4) === '-col'){
+
+                if(!gltf.scene.children[m].children.length > 0){
+                  gltf.scene.children[m].children[0] = gltf.scene.children[m];
+                }
+
+                for (var w = 0; w < gltf.scene.children[m].children.length; w++) {
+                      let vertex = gltf.scene.children[m].children[w].geometry.attributes.position;
+                      let index = gltf.scene.children[m].children[w].geometry.index;
+                      let j, i, ref;
+
+                      let triangles = new Jolt.TriangleList();
+
+                      for (i = j = 0, ref = index.count /  3; (0 <= ref ? j < ref : j > ref); i = 0 <= ref ? ++j : --j) {
+                        triangles.push_back(new Jolt.Triangle(
+                        new Jolt.Vec3(vertex.getX(index.getX(i*3)), vertex.getY(index.getX(i*3)), vertex.getZ(index.getX(i*3))),
+                        new Jolt.Vec3(vertex.getX(index.getY(i*3)), vertex.getY(index.getY(i*3)), vertex.getZ(index.getY(i*3))),
+                        new Jolt.Vec3(vertex.getX(index.getZ(i*3)), vertex.getY(index.getZ(i*3)), vertex.getZ(index.getZ(i*3)))
+                      ));
+                  }
+
+                  let shape = new Jolt.MeshShapeSettings(triangles).Create().Get();
+                  Jolt.destroy(triangles);
+
+                  // Create body
+                  let creationSettings = new Jolt.BodyCreationSettings(shape, new Jolt.Vec3(0, 0, 0), new Jolt.Quat(0, 0, 0, 1), Jolt.EMotionType_Static, LAYER_NON_MOVING);
+                  let body = bodyInterface.CreateBody(creationSettings);
+                  Jolt.destroy(creationSettings);
+                  bodyInterface.AddBody(body.GetID(), Jolt.EActivation_Activate);
+                }
+              }
+
+          }
+
+          setup();
+        },
+        // called while loading is progressing
+        function ( xhr ) {
+        },
+        // called when loading has errors
+        function ( error ) {
+          console.log( 'An error happened: ' + error );
+        }
+      )
     
+      var setup = function(){};
   }
 }
