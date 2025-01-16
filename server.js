1e28df0542eec3af85a8afdeadcbce35a04113b5
terrancybernetics <147798517+terrancybernetics@users.noreply.github.com>
2025-01-15 23:14:49 -0500

    imports updated

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ import express from 'express';
 import ViteExpress from "vite-express";
 import { DedicatedServer } from './dedicatedserver.js';
 import geckos from '@geckos.io/server';
-
+import initJolt from 'jolt-physics';
+import * as THREE from 'three';
+import { Ent } from './ent.js';
+//import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import {GLTFLoader} from 'node-three-gltf';
+              console.log(`aaaaaa`);
 const io = geckos();
-var Dedicated = new DedicatedServer(io);
+var Dedicated = new DedicatedServer(io, initJolt, THREE, Ent, GLTFLoader);
 
 var app = express();
 
